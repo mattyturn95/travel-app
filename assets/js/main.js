a27da40592e5107bdc9b192fedfce99cf49651b8
Stephen Byrne <s.byrne6@gmail.com>
2019-06-04 22:51:24 +0000

    js errors fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,26 +156,6 @@ function search() {
 
 //dine
 
-const myFunctionGo = (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        clearResults();
-        clearMarkers();
-        for (var i = 0; i < results.length; i++) {
-          var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-          var markerIcon = MARKER_PATH + markerLetter + '.png';
-          markers[i] = new google.maps.Marker({
-            position: results[i].geometry.location,
-            animation: google.maps.Animation.DROP,
-            icon: markerIcon
-          });
-          markers[i].placeResult = results[i];
-          google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-          setTimeout(dropMarker(i), i * 100);
-          addResult(results[i], i);
-        }
-      }
-    }
-
 dine.addEventListener("click", function search() {
   
     var search = {
@@ -183,7 +163,30 @@ dine.addEventListener("click", function search() {
     types: ["restaurant", "bar"]
   };
 
-    places.nearbySearch(search, myFunctionGo());
+    places.nearbySearch(search, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      clearResults();
+      clearMarkers();
+      // Create a marker for each hotel found, and
+      // assign a letter of the alphabetic to each marker icon.
+      for (var i = 0; i < results.length; i++) {
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+        // Use marker animation to drop the icons incrementally on the map.
+        markers[i] = new google.maps.Marker({
+          position: results[i].geometry.location,
+          animation: google.maps.Animation.DROP,
+          icon: markerIcon
+        });
+        // If the user clicks a hotel marker, show the details of that hotel
+        // in an info window.
+        markers[i].placeResult = results[i];
+        google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+        setTimeout(dropMarker(i), i * 100);
+        addResult(results[i], i);
+      }
+    }
+  });
 }, false);
 
 
@@ -195,7 +198,30 @@ visit.addEventListener("click", function search() {
     types: ["museum", "park", "zoo", "art_gallery", "church"]
   };
 
-  places.nearbySearch(search, myFunctionGo());
+  places.nearbySearch(search, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      clearResults();
+      clearMarkers();
+      // Create a marker for each hotel found, and
+      // assign a letter of the alphabetic to each marker icon.
+      for (var i = 0; i < results.length; i++) {
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+        // Use marker animation to drop the icons incrementally on the map.
+        markers[i] = new google.maps.Marker({
+          position: results[i].geometry.location,
+          animation: google.maps.Animation.DROP,
+          icon: markerIcon
+        });
+        // If the user clicks a hotel marker, show the details of that hotel
+        // in an info window.
+        markers[i].placeResult = results[i];
+        google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+        setTimeout(dropMarker(i), i * 100);
+        addResult(results[i], i);
+      }
+    }
+  });
 }, false);
 
 
