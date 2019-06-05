26a991c43e6a8256bb58518f978ee48b23146837
Stephen Byrne <s.byrne6@gmail.com>
2019-06-05 18:19:48 +0000

    filters added to main image, how to gif added to intro section along with jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -389,3 +389,29 @@ list.addEventListener('click', function(e) {
     e.target.classList.toggle('checked');
   }
 }, false);
+
+// intro fadeIn
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hidden').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},3000);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
